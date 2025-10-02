28fdabe0e483f474a05aa23c1cc2518a85aa4248
Mariana Herrera Aburto <marianah@lapieza.io>
2025-10-02 02:00:50 -0600

    rutas relativas

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,13 +2,12 @@ const CACHE_NAME = 'pwa-app-shell-v1';
 const RUNTIME = 'pwa-runtime-v1';
 
 const PRECACHE_URLS = [
-  '/',
-  '/index.html',
-  '/css/styles.css',
-  '/js/app.js',
-  '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
+  'index.html',
+  'css/styles.css',
+  'js/app.js',
+  'manifest.json',
+  'icons/icon-192.png',
+  'icons/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
